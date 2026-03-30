f701e73fd2198d2296e87610badeb6ad5f2eb886
Peeperushki <48174614+Peeperushki@users.noreply.github.com>
2026-03-30 20:38:18 +0800

    Added classes

---
+++ src/Classes/ClassCourse.js
@@ -0,0 +1,9 @@
+export class Course {
+    constructor(name, code, creditHour, summary, msTeamsLink) {
+        this.name = name;
+        this.code = code;
+        this.creditHour = creditHour;
+        this.summary = summary;
+        this.msTeamsLink = msTeamsLink;
+    }
+}
